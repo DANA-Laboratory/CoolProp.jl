95870bc78d0895bf56de1a936253a31f727f2db3
reza afzalan <rafzalan@gmail.com>
2016-11-27 15:37:42 +0330

    nightly

--- deps/build.jl
+++ deps/build.jl
@@ -10,8 +10,8 @@ end
 try
   latestVersion_CoolProp = JSON.parse(readstring(download("https://sourceforge.net/projects/coolprop/best_release.json")))["release"]["filename"][11:15];
   println("CoolProp latestVersion = $latestVersion_CoolProp ...")
-  coolpropurlbase = "http://netix.dl.sourceforge.net/project/coolprop/CoolProp/$latestVersion_CoolProp/";
-  _download(coolpropurlbase * "Julia/CoolProp.jl", joinpath(destpathbase, "CoolProp.jl"));
+  coolpropurlbase = "http://www.coolprop.dreamhosters.com/binaries/"
+  _download(coolpropurlbase * "Julia/CoolProp.jl", joinpath(destpathbase,"CoolProp.jl"));
   info("I'm Getting CoolProp Binaries...");
   @static if is_windows()
     urlbase = coolpropurlbase * "shared_library/Windows/$OS_ARCH_CoolProp/";
@@ -20,6 +20,7 @@ try
     _download(joinpath(urlbase,"exports.txt"), joinpath(destpathbase,"exports.txt"));
   end
   @static if is_linux()
+    coolpropurlbase = "http://netix.dl.sourceforge.net/project/coolprop/CoolProp/nightly/";
     urlbase = coolpropurlbase * "shared_library/Linux/$OS_ARCH_CoolProp/libCoolProp.so.$latestVersion_CoolProp";
     _download(urlbase, joinpath(destpathbase,"CoolProp.so"));
   end
